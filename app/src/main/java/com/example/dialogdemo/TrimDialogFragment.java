f7d310bc63b2ccbe2cc68ff46ac4e3934ec768b3
Charles Hoot <hoot@nwmissouri.edu>
2020-03-24 00:12:29 -0500

    Improve the Dialog

--- app/src/main/java/com/example/dialogdemo/TrimDialogFragment.java
+++ app/src/main/java/com/example/dialogdemo/TrimDialogFragment.java
@@ -2,7 +2,9 @@ package com.example.dialogdemo;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +18,14 @@ public class TrimDialogFragment extends DialogFragment {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Do you need a trim?");
+        builder.setMessage("Your input has spaces at the beginning or end.  Do you want to remove those spaces before using the input?");
+        builder.setPositiveButton("Trim the spaces.",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d("Dialog Click", "Positive Dialog Button pressed");
+                    }
+                });
         return builder.create();
     }
 }
